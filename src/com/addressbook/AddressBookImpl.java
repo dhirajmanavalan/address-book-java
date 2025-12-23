4bc2f3d2391bd468dee17a776ff46a1a7c542a44
Dhirajkumar <kumardhiraj7720@gmail.com>
2025-12-23 12:55:04 +0530

    updated code for implementation

--- src/com/addressbook/AddressBookImpl.java
+++ src/com/addressbook/AddressBookImpl.java
@@ -1,4 +1,23 @@
 package com.addressbook;
 
-public class AddressBookImpl {
+import java.util.ArrayList;
+import java.util.List;
+
+public class AddressBookImpl implements AddressBook {
+
+    private List<ContactPerson> contacts;
+
+    public AddressBookImpl() {
+        this.contacts = new ArrayList<>();
+    }
+
+    @Override
+    public void addContact(ContactPerson contact) {
+        contacts.add(contact);
+    }
+
+    @Override
+    public List<ContactPerson> getAllContacts() {
+        return contacts;
+    }
 }
