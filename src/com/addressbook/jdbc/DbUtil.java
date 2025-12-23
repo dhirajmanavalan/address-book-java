d29ea88c60cd8032cd656880d1caa5774d013cd9
Dhirajkumar <kumardhiraj7720@gmail.com>
2025-12-23 14:41:24 +0530

    loadind driver and establishing connection done

--- src/com/addressbook/jdbc/DbUtil.java
+++ src/com/addressbook/jdbc/DbUtil.java
@@ -1,4 +1,17 @@
 package com.addressbook.jdbc;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class DbUtil {
+
+    private static final String URL =
+            "jdbc:mysql://localhost:3306/address_book_db?useSSL=false&serverTimezone=UTC";
+    private static final String USERNAME = "root";
+    private static final String PASSWORD = "your_password";
+
+    public static Connection getConnection() throws SQLException {
+        return DriverManager.getConnection(URL, USERNAME, PASSWORD);
+    }
 }
